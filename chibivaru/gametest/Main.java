64b8b4b5b64694fe795d3b97aca52193d41c5029
chibivaru1225 <chibivaru@live.jp>
2015-11-24 17:52:14 +0900

    no message

---
+++ chibivaru/gametest/Main.java
@@ -0,0 +1,9 @@
+package chibivaru.gametest;
+import javax.swing.JFrame;
+
+class Main{
+  public static void main(String args[]){
+    JFrame frame = new JFrame("Title");
+    frame.setVisible(true);
+  }
+}
